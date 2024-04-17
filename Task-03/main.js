928215ac32dcf54ae0bd47cbbcd5af445d9ad253
Zayanahmed79 <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-17 18:11:01 +0500

    Add files via upload

---
+++ Task-03/main.js
@@ -0,0 +1,4 @@
+var personname = "aoun";
+console.log("Lowercase:", personname.toLowerCase());
+console.log("uppercase:", personname.toUpperCase());
+console.log("Titlecase:", personname.charAt(0).toUpperCase() + personname.slice(1).toLowerCase());
---
+++ Task-03/main.ts
@@ -0,0 +1,7 @@
+let personname: string = "aoun";
+
+console.log("Lowercase:", personname.toLowerCase());
+
+console.log("Uppercase:", personname.toUpperCase());
+
+console.log("Titlecase:", personname.charAt(0).toUpperCase() + personname.slice(1).toLowerCase());
