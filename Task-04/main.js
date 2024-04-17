bc9f79a9c2b8baaede7ec5f0c65de35b567b8e58
Zayanahmed79 <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-17 18:12:40 +0500

    Add files via upload

---
+++ Task-04/main.js
@@ -0,0 +1,3 @@
+var author = "Albert Einstein";
+var quote = "Imagination is more powerful then knowledge";
+console.log("".concat(author, " once said, \"").concat(quote, "\""));
---
+++ Task-04/main.ts
@@ -0,0 +1,4 @@
+let author: string = "Albert Einstein";
+let quote: string = "Imagination is more powerful then knowledge";
+
+console.log(`${author} once said, "${quote}"`);
