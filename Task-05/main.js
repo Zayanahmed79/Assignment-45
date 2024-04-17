b204b67df945f824b50e004869688bfd7b55a169
Zayanahmed79 <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-17 18:17:30 +0500

    Add files via upload

---
+++ Task-05/main.js
@@ -0,0 +1,3 @@
+var famous_person = "Ronaldo";
+var message = "Practice makees the man perfect";
+console.log("".concat(famous_person, " once said,\"").concat(message, "\""));
---
+++ Task-05/main.ts
@@ -0,0 +1,4 @@
+let famous_person: string = "Ronaldo";
+let message: string = "Practice makes the man perfect";
+
+console.log(`${famous_person} once said,"${message}"`);
