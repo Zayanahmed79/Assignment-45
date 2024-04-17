7be6b85e8e3be1c1f037944d1606a7cfb8b0c38b
Zayanahmed79 <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-17 18:07:45 +0500

    Add files via upload

---
+++ Task-02/main.js
@@ -0,0 +1,10 @@
+var personName = "sir zia ullah khan";
+console.log("Lowercase: " + personName.toLowerCase()); //LowerCase
+console.log("Uppercase: " + personName.toUpperCase()); //UpperCase
+// function toTitleCase(str: string): string {
+//     return str.replace(/\w\S*/g, function (txt: string): string {
+//         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+//     });
+// }
+console.log("Titlecase: " + personName.charAt(0).toUpperCase() + personName.substr(1).toLowerCase());
+//TitleCase
