33a037542ab59d4045bd2241dd21da5ed6bc959e
Zayanahmed79 <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-18 17:42:20 +0500

    Add files via upload

---
+++ Task-10/main.js
@@ -0,0 +1,8 @@
+//Name: Zayan Ahmed date:29-03-2024
+//This is First Program =>Addition.It adds up two given values. 
+var add = 2 + 5;
+console.log(add);
+//Name: Zayan Ahmed date:29-03-2024
+//This is Second Program =>Multiplication.It multiplies up two given values. 
+var multiply = 2 * 4;
+console.log(multiply);
---
+++ Task-10/main.ts
@@ -0,0 +1,9 @@
+//Name: Zayan Ahmed date:29-03-2024
+//This is First Program =>Addition.It adds up two given values. 
+let add = 2+5
+console.log(add);
+
+//Name: Zayan Ahmed date:29-03-2024
+//This is Second Program =>Multiplication.It multiplies up two given values. 
+let multiply= 2* 4
+console.log(multiply);
