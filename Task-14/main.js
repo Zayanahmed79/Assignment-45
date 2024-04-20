50200a3b2c6b9a09c959e694befadfc212e52f00
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-20 21:06:57 +0500

    Add files via upload

---
+++ Task-14/main.js
@@ -0,0 +1,2 @@
+var guestlist = ["Shafique", "Sandeed", "Tanzeel", "Aoun"];
+guestlist.forEach(function (val) { return console.log("Hello " + val + ", you are invited to the dinner"); });
---
+++ Task-14/main.ts
@@ -0,0 +1,3 @@
+let guestlist =["Shafique","Sandeed","Tanzeel","Aoun"]
+
+guestlist.forEach((val) => console.log("Hello " + val + ", you are invited to the dinner"))
