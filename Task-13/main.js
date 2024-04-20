d688f26a2360cdf35f9c04bc993c427e5aab4c2e
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-20 21:06:03 +0500

    Add files via upload

---
+++ Task-13/main.js
@@ -0,0 +1,10 @@
+var transport = ["Honda CD 70", "Honda Civic", "Supra", "Nissan GTR"];
+for (var _i = 0, transport_1 = transport; _i < transport_1.length; _i++) {
+    var b = transport_1[_i];
+    console.log("I would like to own a ".concat(b));
+}
+//  transport.forEach((b) => console.log(`I would like to own a ${b}`))
+//  for (let i = 0; i < transport.length ; i++) {
+//     let item = transport[i];
+//      console.log("I would like to own " + item);
+//     }
---
+++ Task-13/main.ts
@@ -0,0 +1,11 @@
+let transport = ["Honda CD 70","Honda Civic","Supra","Nissan GTR"]
+
+ for (let b of transport){
+     console.log(`I would like to own a ${b}`);
+    
+ }
+//  transport.forEach((b) => console.log(`I would like to own a ${b}`))
+//  for (let i = 0; i < transport.length ; i++) {
+//     let item = transport[i];
+//      console.log("I would like to own " + item);
+//     }
