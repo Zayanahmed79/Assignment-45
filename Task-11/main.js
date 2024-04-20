c47ce78bd672d4a123c4450d82c61018d0c2dcbc
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-20 21:04:49 +0500

    Add files via upload

---
+++ Task-11/main.js
@@ -0,0 +1,12 @@
+var names = ["shafique", "sandeed", "fahad", "Nasir"];
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+// for (let a = 0; a < names.length; a++){
+//     console.log(names[a]);
+// }
+// for (let nam of names) {
+//     console.log(nam);
+// }
+names.forEach(function (val) { return console.log(val); });
---
+++ Task-11/main.ts
@@ -0,0 +1,15 @@
+let names:string[] = ["shafique","sandeed","fahad","Nasir"]
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+// for (let a = 0; a < names.length; a++){
+//     console.log(names[a]);
+    
+// }
+
+// for (let nam of names) {
+//     console.log(nam);
+    
+// }
+names.forEach((val) => console.log(val))
