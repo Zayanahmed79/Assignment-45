d3e95d3ba4b781bf0eb3c282eb6513cc40b709e3
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-20 21:05:24 +0500

    Add files via upload

---
+++ Task-12/main.js
@@ -0,0 +1,6 @@
+var names = ["shafique", "sandeed", "fahad", "Nasir"];
+// names.forEach((ass) => console.log(`${ass}, how are you?`))
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var nam = names_1[_i];
+    console.log(nam + ", how are you?");
+}
---
+++ Task-12/main.ts
@@ -0,0 +1,7 @@
+let names:string[] = ["shafique","sandeed","fahad","Nasir"]
+// names.forEach((ass) => console.log(`${ass}, how are you?`))
+for (let nam of names)
+{
+    console.log(nam + ", how are you?");
+    
+}
