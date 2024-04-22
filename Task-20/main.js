4d8342e29165bd770e4ae26fd19be0ec1dff2c4d
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-22 09:16:19 +0500

    Add files via upload

---
+++ Task-20/main.js
@@ -0,0 +1,4 @@
+var cars = ["Supra", "Mustang 1969", "G-Wagon", "Audi A9"];
+var bike = ["Harley Davidson", "Yamaha ", "Honda 70"];
+console.log("I will be ownwer of these cars:".concat(cars));
+console.log("I will be ownwer of these bikes:".concat(bike));
---
+++ Task-20/main.ts
@@ -0,0 +1,4 @@
+let cars:string[] =["Supra","Mustang 1969","G-Wagon","Audi A9"]
+let bike:string[] =["Harley Davidson","Yamaha ","Honda 70"]
+console.log(`I will be ownwer of these cars:${cars}`);
+console.log(`I will be ownwer of these bikes:${bike}`);
