493366a8031085ceb2ee8c2ad1802f51e0b9ec2a
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-22 09:13:50 +0500

    Add files via upload

---
+++ Task-18/main.ts
@@ -0,0 +1,11 @@
+let visplaces = ["Murre","Islamabad","Karachi","Lahore","Sehwan"]
+
+console.log(`Orignal order: ${visplaces}`);
+console.log(`Alphabetical order: ${[...visplaces].sort()}`);
+console.log(`Orignal order: ${visplaces}`);
+console.log(`Revrse alphabetcal order: ${[...visplaces].sort().reverse()}`);
+console.log(`Orignal order: ${visplaces}`);
+console.log(`Reverse order: ${visplaces.reverse()}`);
+
+
+
