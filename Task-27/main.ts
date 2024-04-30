4254debde6b19c476ffab5cd576660f626280f29
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-30 11:31:12 +0500

    Add files via upload

---
+++ Task-27/main.ts
@@ -0,0 +1,9 @@
+//qs.no 27
+let alien_colour = "red";
+if (alien_colour == "green") {
+  console.log("Yes! Alien's colour is green, you just earned 5 points.");
+} else if (alien_colour == "red") {
+  console.log("Yes! Alien's colour is red, you just earned 10 points.");
+} else if (alien_colour == "yellow") {
+  console.log("Yes! Alien's colour is yellow, you just earned 15 points.");
+}
