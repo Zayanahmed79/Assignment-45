abb34cf446ccccf8b624a9a51af635b0e2aee7c8
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-30 11:27:29 +0500

    Add files via upload

---
+++ Task-23/main.js
@@ -0,0 +1,27 @@
+//qs.no 23
+//condition test 1:
+var a = 48;
+var b = 6;
+var result = a > b;
+console.log(result);
+// conditional test 2
+var name1 = "Zayan";
+var name2 = "Shafique";
+var result2 = name1 === name2;
+console.log(result2);
+//test 3
+var carname = "suzuki";
+console.log(carname == "suzuki");
+console.log(carname == "toyota");
+//test 4
+var bikename = "honda";
+console.log(bikename == "honda");
+console.log(bikename == "yamaha");
+//test 5
+var teacher = "Sir Zia Khan";
+console.log(teacher == "Sir Zia Khan");
+console.log(teacher == "Sir Asif Maqbool");
+//test 6
+var Ambani = "RICH";
+console.log(Ambani == "Rich");
+console.log(Ambani == "RICH");
---
+++ Task-23/main.ts
@@ -0,0 +1,38 @@
+//qs.no 23
+//condition test 1:
+let a = 48;
+let b = 6;
+
+let result: boolean = a > b;
+console.log(result);
+
+// conditional test 2
+let name1 = "Zayan";
+let name2 = "Shafique";
+let result2: Boolean = name1 === name2;
+console.log(result2);
+
+//test 3
+let carname = "suzuki";
+
+console.log(carname == "suzuki");
+console.log(carname == "toyota");
+
+//test 4
+let bikename = "honda";
+
+console.log(bikename == "honda");
+console.log(bikename == "yamaha");
+//test 5
+let teacher = "Sir Zia Khan";
+
+console.log(teacher == "Sir Zia Khan");
+console.log(teacher == "Sir Asif Maqbool");
+
+//test 6
+
+let Ambani = "RICH";
+
+console.log(Ambani == "Rich");
+console.log(Ambani == "RICH");
+
