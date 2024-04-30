c55d9e99eb74310fa1040112f4b6d85aefa99f81
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-30 11:32:12 +0500

    Add files via upload

---
+++ Task-28/main.ts
@@ -0,0 +1,18 @@
+//qs.no 28
+let lifesatge = 0
+if (lifesatge < 2){
+    console.log(` IS on Infancy stage`);
+}else if(lifesatge <= 6){
+    console.log(`Is on Early Childhood stage`);
+}else if(lifesatge <= 12){
+    console.log(`Is on Middle childhood stage`);
+}else if(lifesatge <= 18){
+    console.log("Is on Adolescence");
+}else if(lifesatge <= 40){
+    console.log("Is on Early Adulthood stage ");
+}else if(lifesatge <= 60){
+    console.log("IS on Late Middle Adulthood stage");
+}else if (lifesatge > 60){
+    console.log("Is on Late Adulthood stage");   
+}else {
+    console.log("Your entry is not allowed");   }
