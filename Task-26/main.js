aae64fc0495f59a8b8bd8782eb00740eca33b09b
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-30 11:30:16 +0500

    Add files via upload

---
+++ Task-26/main.js
@@ -0,0 +1,8 @@
+//qs.no 26
+var alien_colour2 = "green";
+if (alien_colour2 === "red") {
+    console.log("Yes! Alien's colour is green");
+}
+else {
+    console.log("No! thats not the colour of Alien");
+}
