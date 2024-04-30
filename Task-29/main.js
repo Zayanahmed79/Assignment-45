bdc758dbcb5b406727de3b4e97a7b888370d714b
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-30 11:34:15 +0500

    Add files via upload

---
+++ Task-29/main.js
@@ -0,0 +1,8 @@
+//qs.no 29
+var favfruits = ["Kiwi", "Mangoes", "Apple", "Orange", "Peach"];
+if (favfruits.includes("Kiwi")) {
+    console.log("Yes! this fruit is present in this list.");
+}
+else {
+    console.log("No! this fruit is not present in this list.");
+}
