2deb17edb7f55af0e0c7720200bb61f6764d5558
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-30 11:26:36 +0500

    Add files via upload

---
+++ Task-22/main.ts
@@ -0,0 +1,6 @@
+//qs.no 22
+let array = [12, 2, 3, 53];
+
+console.log(array[4]); // Intentional error: arrays are zero-indexed,this index(4) doesn't exist in the array.
+console.log(array[3]); // Correcting the error by accessing the correct index(3)
+
