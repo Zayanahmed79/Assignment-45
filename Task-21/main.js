c9ad2ceacbac9fb4c1a95775d57e6de3e4555adf
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-30 11:24:35 +0500

    Add files via upload

---
+++ Task-21/main.js
@@ -0,0 +1,6 @@
+var car = {
+    model: 2023,
+    name: "Mustang 1969",
+    prize: 100000
+};
+console.log("Model of car is:".concat(car.model, ",Prize of car is : ").concat(car.prize, ",Name of car is : ").concat(car.name));
---
+++ Task-21/main.ts
@@ -0,0 +1,7 @@
+let car  = {
+    model : 2023,
+    name : "Mustang 1969",
+    prize: 100000
+}
+console.log(`Model of car is:${car.model},Prize of car is : ${car.prize},Name of car is : ${car.name}`);
+
