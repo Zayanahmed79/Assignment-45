676f1535bfba8d15e004873254878ff1d661d942
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-30 11:28:23 +0500

    Add files via upload

---
+++ Task-24/main.js
@@ -0,0 +1,5 @@
+var mycars = ["Civic", "Supra", "G-Wagon"];
+console.log("Do I have Civic? ");
+console.log(mycars.includes("Civic"));
+console.log("Do i have Audi?");
+console.log(mycars.includes("Audi"));
