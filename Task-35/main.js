6454d71c5f6c5b929efcf5efcce67793d5b38f16
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-05-03 19:53:57 +0500

    Add files via upload

---
+++ Task-35/main.js
@@ -0,0 +1,4 @@
+var goodpet = ["Dog", "cat", "Parrot"];
+goodpet.forEach(function (gpet) {
+    console.log("".concat(gpet, " is a good pet. It helps to pass good time.\nIt would be a great pet."));
+});
---
+++ Task-35/main.ts
@@ -0,0 +1,4 @@
+let goodpet = ["Dog","cat","Parrot"]
+goodpet.forEach((gpet)=> {
+    console.log(`${gpet} is a good pet. It helps to pass good time.\nIt would be a great pet.`);
+})
