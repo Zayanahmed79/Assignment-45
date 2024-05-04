fbd818db02257328d0fd65e0d77192fb95d077f8
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-05-04 18:59:57 +0500

    Add files via upload

---
+++ Task-39/index.js
@@ -0,0 +1,6 @@
+function city_country(city, country) {
+    return "".concat(city, " => ").concat(country);
+}
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Islamabad", "Pakistan"));
+console.log(city_country("Beijing", "China"));
---
+++ Task-39/index.ts
@@ -0,0 +1,11 @@
+function city_country(city: string , country : string):string{
+    
+    return `${city} => ${country}`
+
+}
+console.log(city_country("Tokyo","Japan"));
+console.log(city_country("Islamabad","Pakistan"));
+console.log(city_country("Beijing","China"));
+
+
+
