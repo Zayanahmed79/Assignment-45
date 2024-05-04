c6aa90d801c0adad3edbfa25a5a708281c38b507
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-05-04 19:01:26 +0500

    Add files via upload

---
+++ Task-41/index.js
@@ -0,0 +1,6 @@
+function displaymagician(magician) {
+    var b = magician.forEach(function (a) {
+        return console.log(a);
+    });
+}
+displaymagician(["Dumbledore", "Harry Potter"]);
---
+++ Task-41/index.ts
@@ -0,0 +1,5 @@
+function displaymagician(magician : string[]){
+    let b = magician.forEach((a) =>
+        console.log(a))
+}
+displaymagician(["Dumbledore", "Harry Potter"])
